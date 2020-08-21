01592e87d8949e516a51c2981a442c55c503269a
jholkin <jhil.palacios@unas.edu.pe>
2020-08-21 13:14:17 -0500

    proyecto-laravel migrations ralizadas

--- proyecto-laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ proyecto-laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('role');
             $table->string('name');
+            $table->string('surname');
+            $table->string('nick');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
